b0662ef5eb6c5ab6efe83a1026e1a3b92a9d1104
qubilahsade <qhuddles@gmail.com>
2018-01-21 15:05:54 -0500

    added more features

---
+++ javascript.js
@@ -0,0 +1,18 @@
+/* this is a comment */
+console.log('Hello world');
+/*alert('Hello World');*/
+document.write('Hello world');
+
+var  numberOfKittens = 5;
+var numberOfPuppies = 4;
+var numberOfAnimals = number0fKittens + numberOfPuppies
+
+console.log('numberOfKittens ' + numberOfKittens);
+
+console.log(numberOfAnimals);
+
+var kittensname = 'Fluffy';
+
+console.log(kittensname);
+
+console.log("Cat's name" + kittensname);
